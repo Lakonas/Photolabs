9d233811f733047087b0a657e0809de610d8b3c8
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-03-29 00:45:47 -0400

    Corrected photos to make them use the same prop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const photos = new Array(3).fill({
+const sampleDataForPhotoListItem = {
   location: {
     city: "Montreal",
     country: "Canada",
   },
-}).map((photo, index) => ({
-  ...photo,
-  id: (index + 1).toString(),
-  username: `User ${index + 1}`,
-  imageSource: `/Image-${index + 1}-Regular.jpeg`,
-  profile: `/profile-${index + 1}.jpg`,
-}));
+  imageSource: "/Image-1-Regular.jpeg",
+  username: "Joe Example",
+  profile: "/profile-1.jpg",
+};
 
-const App = () => (
-  <div className="App">
-    {photos.map(photo => (
-      <PhotoListItem key={photo.id} photo={photo} />
-    ))}
-  </div>
-);
+const App = () => {
+  const photos = [
+    { ...sampleDataForPhotoListItem, id: "1", username: "Joe Example", imageSource: "/Image-1-Regular.jpeg" },
+    { ...sampleDataForPhotoListItem, id: "2", username: "Jane Example", imageSource: "/Image-2-Regular.jpeg" },
+    { ...sampleDataForPhotoListItem, id: "3", username: "John Example", imageSource: "/Image-3-Regular.jpeg" }
+  ];
+
+  return (
+    <div className="App">
+      {photos.map((photo) => (
+        <PhotoListItem key={photo.id} photo={photo} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
-
-
-
-
