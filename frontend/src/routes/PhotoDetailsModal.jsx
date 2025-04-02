dc938c5bd0073df6615b17371778a5d498c44e8d
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-04-01 20:12:29 -0400

    Fixed favbutton triggring modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,34 @@
-import '../styles/PhotoDetailsModal.scss'
-import closeSymbol from '../assets/closeSymbol.svg';
+import React from 'react';
+import '../styles/PhotoDetailsModal.scss';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ selectedPhoto }) => {
+  if (!selectedPhoto) return null; // If no photo is selected, don't render the modal
+
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
+    <div className="photo-details-modal-overlay">
+      <div className="photo-details-modal">
+        <div className="photo-details-modal__images">
+          <img
+            className="photo-details-modal__image"
+            src={selectedPhoto.urls.full}
+            alt={`Photo by ${selectedPhoto.user.name}`}
+          />
+        </div>
+        <div className="photo-details-modal__photographer-details">
+          <div className="photo-details-modal__photographer-info">
+            <img
+              className="photo-details-modal__photographer-profile"
+              src={selectedPhoto.user.profile}
+              alt={`${selectedPhoto.user.name}'s profile`}
+            />
+            <div className="photo-details-modal__photographer-location">
+              {selectedPhoto.user.name} - {selectedPhoto.location.city}, {selectedPhoto.location.country}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
