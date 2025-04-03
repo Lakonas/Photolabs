e287c14ffebef10ccc356b0c81f4c89b03a56cf1
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-04-03 16:07:01 -0400

    Corrected favbutton not appearong on main photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from '../components/PhotoList'; // Import PhotoList
+import PhotoFavButton from '../components/PhotoFavButton'; // Import PhotoFavButton
 
-const PhotoDetailsModal = ({ selectedPhoto, closeModal }) => { 
+const PhotoDetailsModal = ({ selectedPhoto, closeModal, favoritePhotos, toggleFavorite }) => { 
   if (!selectedPhoto) return null; // If no photo is selected, don't render the modal
   console.log("Similar Photos Data:", selectedPhoto.similar_photos, Array.isArray(selectedPhoto.similar_photos));
+
+  const isFavorited = (favoritePhotos || []).includes(selectedPhoto.id);
 
   return (
     <div className="photo-details-modal-overlay">
@@ -18,17 +21,25 @@ const PhotoDetailsModal = ({ selectedPhoto, closeModal }) => {
 
         <div className="photo-details-modal__images">
           {/* Main Photo */}
+          <div className="photo-details-modal__image">
+             <PhotoFavButton
+                isFavorited={isFavorited}
+                toggleFavorite={() => toggleFavorite(selectedPhoto.id)}
+            />
           <img
-            className="photo-details-modal__image"
-            src={selectedPhoto.urls.full}
-            alt={`Photo by ${selectedPhoto.user.name}`}
-          />
-
+               src={selectedPhoto.urls.full}
+               alt={`Photo by ${selectedPhoto.user.name}`}
+               />
+           </div>
+          
           {/* Similar Photos Section */}
           <div className="photo-details-modal__similar-photos">
             {/* Use PhotoList to render similar photos */}
             <PhotoList 
               photos={Object.values(selectedPhoto.similar_photos)} // Convert object to array
+              favoritePhotos={favoritePhotos}
+              toggleFavorite={toggleFavorite}
+              openModal={() => {}} // No-op function for similar photos in the modal
             />
           </div>
         </div>
