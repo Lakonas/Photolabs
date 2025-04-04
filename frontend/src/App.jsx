0baf7624ee05291dca5303b68c093681aac486c3
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-04-04 19:12:49 -0400

    Glocal state for favorites. Button works globally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import HomeRoute from "./routes/HomeRoute";
 import photos from './mocks/photos'; 
 import topics from './mocks/topics'; 
-import PhotoDetailsModal from './routes/PhotoDetailsModal'; 
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
+import FavIcon from './components/FavIcon'; // Make sure this path is correct
+import FavBadge from './components/FavBadge'; // Make sure this path is correct
 
 const App = () => {
   const [favoritePhotos, setFavoritePhotos] = useState([]);
@@ -11,32 +13,42 @@ const App = () => {
   const toggleFavorite = (photoId) => {
     setFavoritePhotos(prevState => 
       prevState.includes(photoId)
-        ? prevState.filter(id => id !== photoId)  
-        : [...prevState, photoId]  
+        ? prevState.filter(id => id !== photoId)
+        : [...prevState, photoId]
     );
   };
 
   const openModal = (photo) => {
-    setSelectedPhoto(photo); 
+    setSelectedPhoto(photo);
   };
 
   const closeModal = () => {
-    setSelectedPhoto(null); 
+    setSelectedPhoto(null);
   };
 
   return (
     <div className="App">
+
+      {/* Global favorite notification icons */}
+      <FavIcon displayAlert={favoritePhotos.length > 0} />
+      <FavBadge isFavPhotoExist={favoritePhotos.length > 0} />
+
       <HomeRoute 
-        photos={photos} 
-        topics={topics} 
-        favoritePhotos={favoritePhotos} 
+        photos={photos}
+        topics={topics}
+        favoritePhotos={favoritePhotos}
         toggleFavorite={toggleFavorite}
-        openModal={openModal} 
+        openModal={openModal}
       />
 
-      {/* Show the modal only when a photo is selected */}
+      {/* Modal for photo details */}
       {selectedPhoto && (
-        <PhotoDetailsModal selectedPhoto={selectedPhoto} closeModal={closeModal} /> 
+        <PhotoDetailsModal
+          selectedPhoto={selectedPhoto}
+          closeModal={closeModal}
+          favoritePhotos={favoritePhotos}
+          toggleFavorite={toggleFavorite}
+        />
       )}
     </div>
   );
